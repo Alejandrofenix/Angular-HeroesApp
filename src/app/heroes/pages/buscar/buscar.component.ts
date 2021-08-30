b5b66677fe8f6be5cee723faab54f665df9eb28f
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-29 22:13:06 -0500

    Se añadio el servicio al buscador

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -10,25 +10,36 @@ import { HeroesService } from '../../services/heroes.service';
   ]
 })
 export class BuscarComponent implements OnInit {
-  termino:string = "";
-  heroes:Heroe[]=[];
-  heroeSelected!:Heroe;
 
-  constructor( private heroesService: HeroesService) { }
+  termino: string  = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado: Heroe | undefined;
+
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+  }
+
+
+  buscando() {
+
+    this.heroesService.getSugerencias( this.termino.trim() )
+      .subscribe( heroes => this.heroes = heroes );
 
   }
 
-  buscando(){
-    this.heroesService.getSugerencias(this.termino).subscribe(heroes=> this.heroes=heroes);
-  }
+  opcionSeleccionada( event: MatAutocompleteSelectedEvent ) {
 
-  opcionSeleccionada(event:MatAutocompleteSelectedEvent){
+    if(!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
-    this.termino=heroe.superhero;
+    this.termino = heroe.superhero;
 
-    this.heroesService.getHeroeById(heroe.id!).subscribe(heroe=>this.heroeSelected=heroe);
+    this.heroesService.getHeroeById( heroe.id! )
+      .subscribe( heroe => this.heroeSeleccionado = heroe );
   }
 
 }

e5b092c48729022a3e93e95745bcb09a95b9bcd5
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-29 20:20:52 -0500

    Se agregó la interfaz para el componente de busqueda

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -13,14 +16,17 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
 
   exports: [
-    MatSidenavModule,
-    MatToolbarModule,
+    MatAutocompleteModule,
     MatButtonModule,
-    MatIconModule,
-    MatListModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
     MatProgressSpinnerModule,
-    MatGridListModule
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }

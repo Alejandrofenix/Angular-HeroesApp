536e303bd168d48ae2544a2e39b9403cd9292f65
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-30 23:49:01 -0500

    Se agragó la vista para agregar Héroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AgregarComponent implements OnInit {
+  publishers= [
+    {
+      id:'DC Comics',
+      desc:'DC'
+    },
+    {
+      id:'Marvel Comics',
+      desc:'Marvel'
+    }
+    
+  ];
 
+  heroe: Heroe={
+    superhero:'',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+     publisher: Publisher.DCComics,
+     alt_img:''
+
+  }
   constructor() { }
 
   ngOnInit(): void {

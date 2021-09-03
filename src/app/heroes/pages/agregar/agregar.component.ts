b10a77acb083920b8e288ad14d2810a478b23213
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-02 23:49:19 -0500

    Se agrego el service para actualizar un héroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-agregar',
@@ -31,9 +33,17 @@ export class AgregarComponent implements OnInit {
 
   }
   
-  constructor(private heroesService: HeroesService) { }
+  //Activated sirve para leer la url
+  constructor(private heroesService: HeroesService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+    .pipe( 
+      switchMap(({id})=>this.heroesService.getHeroeById(id))
+    )
+    .subscribe(heroe=>this.heroe=heroe);
+
   }
 
   guardar(){
@@ -42,7 +52,16 @@ export class AgregarComponent implements OnInit {
       return;
     }
 
-    this.heroesService.agregarHeroe(this.heroe).subscribe(resp=>console.log(resp));
+    if(this.heroe.id){
+      //Actualizar
+      
+    } else{
+      //Crear
+      this.heroesService.agregarHeroe(this.heroe).subscribe(resp=>console.log(resp));
+    }
+    
+
+
     
     
   }

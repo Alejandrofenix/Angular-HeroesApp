e2cf83bba403da299502ae4b273da5a19af9d315
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-05 20:29:05 -0500

    Se agrego un AuthGuard con el metodo canLoad

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,17 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private _url: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth():Auth {
+    return { ...this._auth! }; //Se destructura con el ... para asegurarnos de que no se cambiaran los valores
+  }
+
   constructor(private http: HttpClient) { }
 
-  login(){
-    return this.http.get<Auth>(`${this._url}/usuarios/1`);
+  login() {
+    return this.http.get<Auth>(`${this._url}/usuarios/1`)
+      .pipe(
+        tap(auth => this._auth = auth)
+      );
 
-    
+
+
   }
 }

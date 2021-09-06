0881e01fc8a0c15c9edd2fc7bbe4a9faa7999c3a
Alejandro Pinedo B <apinedobarrios@live.com>
2021-09-05 21:39:06 -0500

    Se agregó la persitencía con las Guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,31 +1,49 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements  CanLoad {
+export class AuthGuard implements CanLoad {
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean  {
-      if(this.authService.auth.id){
-        return true;
-      }  
-      console.log('Bloqueado por el AuthGuard Actiavate');
-      
-      return false;
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+    return this.authService.verificaAutenticacion().pipe(
+      tap(estaAutenticado => {
+        if (!estaAutenticado) {
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    );
+    // if(this.authService.auth.id){
+    //   return true;
+    // }  
+    // console.log('Bloqueado por el AuthGuard Actiavate');
+
+    // return false;
   }
-  constructor(private authService:AuthService){}
+  constructor(private authService: AuthService, private router: Router) { }
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean>| boolean  {
-     if(this.authService.auth.id){
-       return true;
-     }  
-     console.log('Bloqueado por el AuthGuard Load');
-     return false;
-      
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+
+    return this.authService.verificaAutenticacion().pipe(
+      tap(estaAutenticado => {
+        if (!estaAutenticado) {
+          this.router.navigate(['./auth/login']);
+        }
+      })
+    );
+    //  if(this.authService.auth.id){
+    //    return true;
+    //  }  
+    //  console.log('Bloqueado por el AuthGuard Load');
+    //  return false;
+
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from "rxjs/operators";
+import { Observable, of} from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
-import { tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +18,22 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  verificaAutenticacion():Observable<boolean>{
+    if(!localStorage.getItem('token')){
+      return of(false);//Resuleve en un obseravle el valor que le ponemos
+    }
+    return this.http.get<Auth>(`${this._url}/usuarios/1`).pipe(
+      map( auth=> {
+        return true;
+      } )
+    );
+  }
+
   login() {
     return this.http.get<Auth>(`${this._url}/usuarios/1`)
       .pipe(
-        tap(auth => this._auth = auth)
+        tap(auth => this._auth = auth),
+        tap(auth => localStorage.setItem('id',auth.id)) //Se graba el auth.id en el local storage
       );
 
 
